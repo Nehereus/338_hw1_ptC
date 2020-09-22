014430e878f026485c515608dbf65f7bf672feae
nehereus <934302433@qq.com>
2020-09-22 19:58:06 -0400

    typeB_0.2

--- src/typeA.java
+++ src/typeA.java
@@ -1,4 +1,4 @@
-class TypeA {
+class TypeB {
     static ReadyQueue rq;
 
     static boolean runATask(Process p) {
@@ -10,14 +10,20 @@ class TypeA {
 
 
     public static void main(String[] args) {
+        //every round is a simulation for a cpu slice
         boolean b;
         rq.sort();
         while (rq.getStatus()) {
+            // read the top task from the memory and run it for a slice
             b = runATask(rq.getFirst());
             if (b) {
+                // dump the finished process from the memory
                 rq.memoryCollector();
+
             }
+            // a cpu slice end, the arrival time of each task reduce 1.
             rq.clock();
+            // for typeB, the only difference from A is that the queue is sorted every time slice even though the current task has not been finished yet.
             rq.sort();
         }
     }
